66f70d628ff8727b2c7ed316aa04719cb00be5b5
freakynoblegas <nickquinn3131@gmail.com>
2025-05-09 16:08:06 -0400

    Updated snacks explorer to show all files and directories.

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,8 +2,11 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      -- Configure Snacks explorer
+      -- https://github.com/folke/snacks.nvim/blob/main/docs/explorer.md
       picker = {
-        hidden = true, -- for hidden files
+        hidden = true,
+        ignored = true,
       },
     },
   },
