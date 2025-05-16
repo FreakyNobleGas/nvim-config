cd20c4ee2455716a994813e14b23101e07c510fc
freakynoblegas <nickquinn3131@gmail.com>
2025-05-16 09:24:09 -0400

    Added Mason plugins

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "basedpyright",
+        "bash-language-server",
+        "black",
+        "gofumpt",
+        "goimports",
+        "gopls",
+        "json-lsp",
+        "lua-language-server",
+        "markdown-toc",
+        "markdownlint-cli2",
+        "marksman",
+        "pyright",
+        "ruff",
+        "shellcheck",
+        "shfmt",
+        "stylua",
+        "yaml-language-server",
+      },
+    },
+  },
+}
