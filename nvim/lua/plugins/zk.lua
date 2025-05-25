d468a0a3989ea4a67861a0c1582e3cf23238a133
freakynoblegas <nickquinn3131@gmail.com>
2025-05-25 07:32:48 -0400

    Fixed zk

--- nvim/lua/plugins/zk.lua
+++ nvim/lua/plugins/zk.lua
@@ -1,18 +1,26 @@
 return {
-  {
-    "zk-org/zk-nvim",
-    config = function()
-      require("zk").setup({
-        lsp = {
-          -- Enable automatic attachment of ZK's LSP - Enable automatic attachment of ZK's LSP - Enable automatic attachment of ZK's LSP - Enable automatic attachment of ZK's LSP - Enable automatic attachment of ZK's LSP - Enable automatic attachment of ZK's LSP
-          auto_attach = {
-            enabled = true,
-            filetypes = { "markdown" },
-          },
+  "zk-org/zk-nvim",
+  config = function()
+    require("zk").setup({
+      -- Can be "telescope", "fzf", "fzf_lua", "minipick", "snacks_picker",
+      -- or select" (`vim.ui.select`).
+      picker = "select",
+
+      lsp = {
+        -- `config` is passed to `vim.lsp.start(config)`
+        config = {
+          name = "zk",
+          cmd = { "zk", "lsp" },
+          filetypes = { "markdown" },
+          -- on_attach = ...
+          -- etc, see `:h vim.lsp.start()`
         },
-        -- Optional: Use Telescope for selecting notes
-        picker = "telescope",
-      })
-    end,
-  },
+
+        -- automatically attach buffers in a zk notebook that match the given filetypes
+        auto_attach = {
+          enabled = true,
+        },
+      },
+    })
+  end,
 }
